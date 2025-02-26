e7c32e2aed9a6e8e7812a388eee254f5609ff63e
NickusTop <148118087+NickusTop@users.noreply.github.com>
2025-02-26 17:56:22 +0200

    TT

---
+++ script.js
@@ -0,0 +1,20 @@
+let index = 0;
+        const slides = document.querySelector('.slides');
+        const totalSlides = document.querySelectorAll('.slide').length;
+
+        function nextSlide() {
+            index = (index + 1) % totalSlides;
+            slides.style.transform = `translateX(-${index * 100}%)`;
+        }
+        setInterval(nextSlide, 5000);
+        document.getElementById('languageSelect').addEventListener('change', function() {
+            const language = this.value;
+            if (language === 'en') {
+              window.location.href = 'index.html'; // замените на нужный файл
+            } else if (language === 'ru') {
+              window.location.href = 'index-ru.html'; // замените на нужный файл
+            } else if (language === 'es') {
+              window.location.href = 'index-ua.html'; // замените на нужный файл
+            }
+          });
+          
