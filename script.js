6a85a01d4ddae9ca3667055bee78bbe6ed0f62ea
NickusTop <148118087+NickusTop@users.noreply.github.com>
2025-02-26 17:59:40 +0200

    ЕЕЕ

--- script.js
+++ script.js
@@ -7,14 +7,25 @@ let index = 0;
             slides.style.transform = `translateX(-${index * 100}%)`;
         }
         setInterval(nextSlide, 5000);
-        document.getElementById('languageSelect').addEventListener('change', function() {
-            const language = this.value;
-            if (language === 'en') {
-              window.location.href = 'index.html'; // замените на нужный файл
-            } else if (language === 'ru') {
-              window.location.href = 'index-ru.html'; // замените на нужный файл
-            } else if (language === 'es') {
-              window.location.href = 'index-ua.html'; // замените на нужный файл
-            }
-          });
+        // Устанавливаем выбранное значение из localStorage при загрузке страницы
+  window.onload = function() {
+    const savedLanguage = localStorage.getItem('selectedLanguage');
+    if (savedLanguage) {
+      document.getElementById('languageSelect').value = savedLanguage;
+    }
+  };
+
+  document.getElementById('languageSelect').addEventListener('change', function() {
+    const language = this.value;
+    // Сохраняем выбранный язык в localStorage
+    localStorage.setItem('selectedLanguage', language);
+    
+    if (language === 'en') {
+      window.location.href = 'index.html'; // замените на нужный файл
+    } else if (language === 'ru') {
+      window.location.href = 'index-ru.html'; // замените на нужный файл
+    } else if (language === 'es') {
+      window.location.href = 'index-ua.html'; // замените на нужный файл
+    }
+  });
           
